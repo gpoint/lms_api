f1595e36e62a727882d46466e877f0b837d1eca2
Chibueze Harry <chibuezeharry@Chibuezes-MacBook-Air.local>
2020-11-10 14:16:19 +0100

    Removing Node Modules

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const { promisify } = require('util')
 const app = express()
-const port = 3000
+
+app.use(bodyParser.json())
+
+// const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
+const startServer = async () => {
+  const port = process.env.SERVER_PORT || 3000
+  await promisify(app.listen).bind(app)(port)
+  console.log(`Listening on port ${port}`)
+}
+
+startServer()
